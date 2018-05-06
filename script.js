0d67df7b5fc7535987bd1d19c611da645c87a37c
John <ristaujo@gmail.com>
2018-05-06 19:34:58 -0400

    Almost there

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ while (true) {
   }
 } */
 
-window.onload = function() {
+/* window.onload = function() {
   //Button
   let AddBtn = document.getElementById("addContact");
   //Form Fields
@@ -62,8 +62,7 @@ window.onload = function() {
   let relation = document.getElementById("relation");
 
   //Display Bottom Boxes
-  let added_contacts_section = document.querySelector(".added_contacts");
-
+  let addBookDiv = document.querySelector('.addbook');
   //Create Storage Array
   let addressBook = [];
 
@@ -86,6 +85,7 @@ window.onload = function() {
       localStorage['addBook'] = JSON.stringify(addressBook);
       // Clear form
       clearForm();
+      showContacts();
       // Updating and displaying all records in address book
     }
   }
@@ -97,22 +97,104 @@ window.onload = function() {
   }
   function showContacts (){
     if(localStorage['addbook'] === undefined){
-      localStorage['addbook'] = "[]";
+      localStorage['addbook'] = '';
     } else {
       addressBook = JSON.parse(localStorage['addbook']);
       //empty out added_contacts
-      added_contacts_section.innerHTML = '';
+      addBookDiv.innerHTML = '';
       for (let i in addressBook) {
-        let str = '<section class="entry">';
+        let str = '<section class="addbook">';
             str += '<div class="name"><p>' + addressBook[i].name + '</p></div>';
             str += '<div class="email"><p>' + addressBook[i].email + '</p></div>';
             str += '<div class="phone"><p>' + addressBook[i].phone + '</p></div>';
             str += '<div class="relation"><p>' + addressBook[i].relation + '</p></div>';
             str += '<div class="del"><a href="#" class="delbutton" data-id="' + i + '">Delete</a></div>';
-            str += '</div>';
-            added_contacts_section.innerHTML += str;
+            str += '</section>';
+            addBookDiv.innerHTML += str;
       }
     }
   }
   showContacts();
+} */
+
+window.onload = function(){
+	// Buttons
+	var AddBtn = document.getElementById('addContact');
+	// Form Fields
+	var name = document.getElementById('name');
+	var email = document.getElementById('email');
+	var phone = document.getElementById('phone');
+	var relation = document.getElementById('relation');
+	// Divs etc.
+	var addBookDiv = document.querySelector('.addbook');
+
+	AddBtn.addEventListener("click", addToBook);
+
+	addBookDiv.addEventListener("click", removeEntry);
+
+	// Storage Array
+	var addressBook = [];
+
+	//localStorage['addbook'] = '[{"name":"Sachin B","email":"sachin@frameboxx.in","phone":"93828292","address":"something","city":"Chandigarh"}]';
+
+	function jsonStructure(name,email,phone,relation){
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.relation = relation
+	}
+
+	function addToBook(){
+		var isNull = name.value!='' && email.value!='' && phone.value!='' && relation.value!='';
+		if(isNull){
+			// format the input into a valid JSON structure
+			var obj = new jsonStructure(name.value,email.value,phone.value,relation.value);
+			addressBook.push(obj);
+			localStorage['addbook'] = JSON.stringify(addressBook);
+			clearForm();
+			showAddressBook();
+		}
+	}
+
+	function removeEntry(e){
+		// Remove an entry from the addressbook
+		if(e.target.classList.contains('delbutton')){
+			var remID = e.target.getAttribute('data-id');
+			addressBook.splice(remID,1);
+			localStorage['addbook'] = JSON.stringify(addressBook);
+			showAddressBook();
+		}
+	}
+
+	function clearForm(){
+		var myinputs = document.querySelectorAll('.myinputs');
+		for(var i in myinputs){
+			myinputs[i].value = '';
+		}
+	}
+
+	function showAddressBook(){
+		if(localStorage['addbook'] === undefined){
+			localStorage['addbook'] = '';
+		} else {
+			addressBook = JSON.parse(localStorage['addbook']);
+			// Loop over the array addressBook and insert into the page
+			addBookDiv.innerHTML = '';
+			for(var n in addressBook){
+				var str = '<section class="entry">';
+					str += '<div class="name"><p>' + addressBook[n].name + '</p></div>';
+					str += '<div class="email"><p>' + addressBook[n].email + '</p></div>';
+					str += '<div class="phone"><p>' + addressBook[n].phone + '</p></div>';
+					str += '<div class="address"><p>' + addressBook[n].address + '</p></div>';
+					str += '<div class="city"><p>' + addressBook[n].city + '</p></div>';
+					str += '<div class="del"><a href="#" class="delbutton" data-id="' + n + '">Delete</a></div>';
+					str += '</section>';
+				addBookDiv.innerHTML += str;
+			}
+		}
+	}
+
+	showAddressBook();
+
 }
+
